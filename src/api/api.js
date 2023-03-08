9c8aca67015416606c9ec104a844b26e51460d58
Alessadro Esposito Brescia <alessandro.esposito.brescia@gmail.com>
2023-03-08 20:50:05 -0300

    changed production link

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const apiURLs = {
     development: "http://localhost:4000",
-    production: "https://yogahome.cyclic.app"
+    production: "https://yogahome.netlify.app"
 }
 
 const api = axios.create({baseURL: apiURLs[process.env.NODE_ENV]})
