f00fa47eeb9c251f59819d908a3301c7256fdce3
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2023-04-08 22:12:31 +0200

    update navbar reducing links

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -54,7 +54,6 @@ export function NavBar() {
   const navigation = [
     { name: "Home", href: "/", current: true, hide: false },
     { name: "Practice", href: "/practice", current: false, hide: false },
-    { name: "Profile", href: "/profile", current: false, hide: !loggedInUser },
     {
       name: "My Classes",
       href: "/order/my-orders",
@@ -191,14 +190,14 @@ export function NavBar() {
                               <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                                 <Menu.Item key="Settings">
                                   {({ active }) => (
-                                    <Link to="/profile/settings">
+                                    <Link to="/profile">
                                       <div
                                         className={classNames(
                                           active ? "bg-gray-100" : "",
                                           "block px-4 py-2 text-sm text-gray-700"
                                         )}
                                       >
-                                        Edit profile
+                                        Profile
                                       </div>
                                     </Link>
                                   )}
